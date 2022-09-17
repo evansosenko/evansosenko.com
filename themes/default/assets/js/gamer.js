dd27e4a169785f2b3af6ce187a34d64f516b81a9
Evan Sosenko <razorx@evansosenko.com>
2022-09-17 17:43:42 +0000

    Setup game data

--- themes/default/assets/js/gamer.js
+++ themes/default/assets/js/gamer.js
@@ -1,22 +1,36 @@
-const initHandlers = (document) => {
-  const elements = document.querySelectorAll('.gamer-button')
-  for (const element of elements) initHandler(document, element)
-}
+const gameData = [
+  {
+    icon: 'alien-8bit-solid',
+    sound:
+      '{{ (resources.Get "audio/shoot.wav" | resources.Fingerprint).RelPermalink }}'
+  }
+]
 
-const initHandler = (document, element) => {
+const initHandlers = (document) => {
   const Howl = globalThis?.Howl
   if (Howl == null) return
+  const elements = document.querySelectorAll('.gamer-button')
+  if (elements.length === 0) return
+  const games = gameData.map(createGame)
+  for (const element of elements) initHandler(document, element, games)
+}
 
-  const sound = new Howl({
+const createGame = ({ sound, ...rest }) => ({
+  sound: createSound(Howl, sound),
+  ...rest
+})
+
+const createSound = (Howl, url) => {
+  return new Howl({
     preload: true,
-    src: [
-      '{{ (resources.Get "audio/shoot.wav" | resources.Fingerprint).RelPermalink }}'
-    ]
+    src: [url]
   })
+}
 
+const initHandler = (document, element, games) => {
   element.addEventListener('click', (event) => {
     event.preventDefault()
-    sound.play()
+    games[0].sound.play()
   })
 }
 
